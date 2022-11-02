3708a2a7b72964351731534eb4e7563122c90146
Nicole <nicole.georgievva@gmail.com>
2022-11-02 23:20:23 +0200

    Exercise variable types

--- Test/src/main/java/com/example/test/MyClass.kt
+++ Test/src/main/java/com/example/test/MyClass.kt
@@ -1,5 +1,8 @@
 package com.example.test
 
 fun main() {
-    print("Hello world!")
+    val name: String = "Nicole"
+    val age: Int = 23
+
+    print("Hello, my name is $name and I'm $age years old.")
 }
