e1f8d97d081fa6e9f8a5196c26da80579d457f95
Nicole <nicole.georgievva@gmail.com>
2022-11-03 20:22:22 +0200

    Exercise Functions

--- Test/src/main/java/com/example/test/MyClass.kt
+++ Test/src/main/java/com/example/test/MyClass.kt
@@ -1,8 +1,13 @@
 package com.example.test
 
 fun main() {
-    for (i in 1..1000)
-        if (i % 3 == 0)
-            println("$i is multiple of 3")
-        else println("---")
+    sayHello()
+}
+
+fun sayHello() {
+    for (i in 1..100) {
+        if (i % 2 == 0) {
+            println("$i is multiple of 2")
+        }
+    }
 }
