181fe619dc21fba57f894d06c223f6aafba13b30
Nicole <nicole.georgievva@gmail.com>
2022-11-03 20:08:16 +0200

    Exercise For Loop

--- Test/src/main/java/com/example/test/MyClass.kt
+++ Test/src/main/java/com/example/test/MyClass.kt
@@ -1,14 +1,6 @@
 package com.example.test
 
 fun main() {
-    val amount = 999
-
-    when (amount) {
-        in 1..100 -> print("This amount is between 1 and 100.")
-        !in 10..90 -> print("This amount is outside the range.")
-        999 -> print("Really close.")
-        1000 -> print("Rich but not there.")
-        1100 -> print("You've made it.")
-        else -> print("$amount is just not going to work.")
-    }
+   for (i in 5..8)
+       println(i)
 }
