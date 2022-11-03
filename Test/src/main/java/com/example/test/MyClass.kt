aef6d78235c80444ca81cb39152f51757c307d7e
Nicole <nicole.georgievva@gmail.com>
2022-11-03 21:05:04 +0200

    Exercise default arguments

--- Test/src/main/java/com/example/test/MyClass.kt
+++ Test/src/main/java/com/example/test/MyClass.kt
@@ -1,15 +1,13 @@
 package com.example.test
 
 fun main() {
-    multipleOf(1, 100, "Action")
+    multipleOf(message = "is multiple of", multipleOf = 11)
 }
 
-fun multipleOf(first: Int, second: Int, genre: String) {
+fun multipleOf(first: Int = 1, second: Int = 1000, message: String, multipleOf: Int) {
     for (i in first..second) {
-        if (i % 2 == 0 && genre == "Action") {
-            println("Number $i is an action movie multiple of 2.")
-        } else if (genre !== "Action"){
-            println(genre)
+        if (i % multipleOf == 0) {
+            println("$i $message $multipleOf")
         }
     }
 }
