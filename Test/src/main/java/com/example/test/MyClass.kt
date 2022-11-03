d61e32d81a670c1f5ef6fcc2388d3911a7e5d49c
Nicole <nicole.georgievva@gmail.com>
2022-11-03 20:32:15 +0200

    Exercise Functions with Int Parameters

--- Test/src/main/java/com/example/test/MyClass.kt
+++ Test/src/main/java/com/example/test/MyClass.kt
@@ -1,11 +1,11 @@
 package com.example.test
 
 fun main() {
-    multipleOf2()
+    multipleOf2(1, 1000)
 }
 
-fun multipleOf2() {
-    for (i in 1..100) {
+fun multipleOf2(first: Int, second: Int) {
+    for (i in first..second) {
         if (i % 2 == 0) {
             println("$i is multiple of 2")
         }
