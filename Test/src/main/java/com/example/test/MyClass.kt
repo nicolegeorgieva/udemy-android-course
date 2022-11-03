7b594265d6592173b0d99fd888b09ad26a552abf
Nicole <nicole.georgievva@gmail.com>
2022-11-03 18:57:12 +0200

    Exercise When Expression - Range

--- Test/src/main/java/com/example/test/MyClass.kt
+++ Test/src/main/java/com/example/test/MyClass.kt
@@ -1,16 +1,14 @@
 package com.example.test
 
 fun main() {
-    val amount = 900
+    val amount = 999
 
-    when(amount) {
-        100 -> print("You have 100.")
-        125 -> print("You have 125.")
+    when (amount) {
+        in 1..100 -> print("This amount is between 1 and 100.")
+        !in 10..90 -> print("This amount is outside the range.")
         999 -> print("Really close.")
         1000 -> print("Rich but not there.")
         1100 -> print("You've made it.")
-        else -> {
-            print("$amount is just not going to work.")
-        }
+        else -> print("$amount is just not going to work.")
     }
 }
