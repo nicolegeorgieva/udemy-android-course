cb8e6ae5c196f91f753a97164e762061e0abf643
Nicole <nicole.georgievva@gmail.com>
2022-11-05 23:13:58 +0200

    Exercise collections

--- Test/src/main/java/com/example/test/MyClass.kt
+++ Test/src/main/java/com/example/test/MyClass.kt
@@ -1,17 +1,9 @@
 package com.example.test
 
 fun main() {
-    say(23) { age -> age >= 18 }
-    say(9) { age -> age >= 21 }
+    val myListOfNames = listOf("Nicole", "Iliyan", "Amy")
+    print(myListOfNames)
 }
 
-fun say(age: Int, isAdult: (Int) -> Boolean) {
-    val result = isAdult(age)
 
-    if(result) {
-        println("You're adult.")
-    } else {
-        println("You're young.")
-    }
-}
 
