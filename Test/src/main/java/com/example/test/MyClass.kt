94b60aacd0181f07d7d1ca7980503c66b851eb13
Nicole <nicole.georgievva@gmail.com>
2022-11-05 23:54:12 +0200

    Exercise map collection

--- Test/src/main/java/com/example/test/MyClass.kt
+++ Test/src/main/java/com/example/test/MyClass.kt
@@ -2,9 +2,8 @@ package com.example.test
 
 fun main() {
     val secretMap = mapOf("Up" to 1, "Down" to 2, "Left" to 3, "Right" to 4)
-    println(secretMap)
-    println(secretMap.keys)
-    println(secretMap.values)
+
+    if ("Left" in secretMap) print("Yes, it's in.")
 }
 
 
