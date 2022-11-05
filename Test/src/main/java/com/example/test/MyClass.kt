b41e6a90269a7d918585ae35c9c01816cceb08bc
Nicole <nicole.georgievva@gmail.com>
2022-11-05 23:16:54 +0200

    Exercise collections

--- Test/src/main/java/com/example/test/MyClass.kt
+++ Test/src/main/java/com/example/test/MyClass.kt
@@ -2,7 +2,9 @@ package com.example.test
 
 fun main() {
     val myListOfNames = listOf("Nicole", "Iliyan", "Amy")
-    print(myListOfNames)
+    for (name in myListOfNames) {
+        println("Hi, $name!")
+    }
 }
 
 
