a1a7c8314823bfb3cda5a4f2cab7ad483b1725f0
Nicole <nicole.georgievva@gmail.com>
2022-11-05 23:36:06 +0200

    Exercise mutable collection using methods

--- Test/src/main/java/com/example/test/MyClass.kt
+++ Test/src/main/java/com/example/test/MyClass.kt
@@ -3,7 +3,8 @@ package com.example.test
 fun main() {
     val myMutableList = mutableListOf(5, 4, 3, 7)
 
-    print("The number of elements is ${myMutableList.size}.")
+    println("The number of elements is ${myMutableList.size}.")
+    println("The second element is ${myMutableList.get(1)}.")
 }
 
 
