12db860dbd13826a32e09385a97189073727fd19
Nicole <nicole.georgievva@gmail.com>
2022-11-05 23:33:33 +0200

    Exercise mutable collection using methods

--- Test/src/main/java/com/example/test/MyClass.kt
+++ Test/src/main/java/com/example/test/MyClass.kt
@@ -1,14 +1,9 @@
 package com.example.test
 
 fun main() {
-//    val myListOfNames = listOf("Nicole", "Iliyan", "Amy")
     val myMutableList = mutableListOf(5, 4, 3, 7)
 
-    myMutableList.add(6)
-
-    myMutableList.removeAt(1)
-
-    print(myMutableList)
+    print("The number of elements is ${myMutableList.size}.")
 }
 
 
