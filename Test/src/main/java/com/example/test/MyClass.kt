5d724b685e11ba23d95a47584c8778f56ea1cc21
Nicole <nicole.georgievva@gmail.com>
2022-11-06 17:11:37 +0200

    Exercise init blocks

--- Test/src/main/java/com/example/test/MyClass.kt
+++ Test/src/main/java/com/example/test/MyClass.kt
@@ -2,13 +2,14 @@ package com.example.test
 
 fun main() {
     val car = Car(color = "Red", model = "jsdlf")
-
-    print("${car.color} ${car.model}")
 }
 
 class Car(var color: String, var model: String) {
     init {
-        color = "Yellow"
-        model = "dgdfl"
+        if (color == "Green") {
+            println("Yayy Green!")
+        } else {
+            println("$color is not Green.")
+        }
     }
 }
