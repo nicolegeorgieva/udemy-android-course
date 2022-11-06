5e453261ca5824c5fe98ab97b8e2f045d7ac6393
Nicole <nicole.georgievva@gmail.com>
2022-11-06 16:32:55 +0200

    Create first class

--- Test/src/main/java/com/example/test/MyClass.kt
+++ Test/src/main/java/com/example/test/MyClass.kt
@@ -1,14 +1,11 @@
 package com.example.test
 
 fun main() {
-    val myListOfNames = listOf("Nicole", "Iliyan", "Amy")
-
-    val found = myListOfNames.filter {
-        it.startsWith("a", ignoreCase = true) && it.endsWith("y")
-    }
-
-    print(found)
+    val car = Car()
+    print(car.color)
 }
 
-
+class Car {
+    val color: String = "Red"
+}
 
