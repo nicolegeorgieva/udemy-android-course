becfbcea6dec76f2593cebe33575ba3685171ab9
Nicole <nicole.georgievva@gmail.com>
2022-11-06 16:15:18 +0200

    Exercise collection filters

--- Test/src/main/java/com/example/test/MyClass.kt
+++ Test/src/main/java/com/example/test/MyClass.kt
@@ -4,7 +4,7 @@ fun main() {
     val myListOfNames = listOf("Nicole", "Iliyan", "Amy")
 
     val found = myListOfNames.filter {
-        it.length > 4
+        it.endsWith("e")
     }
 
     print(found)
