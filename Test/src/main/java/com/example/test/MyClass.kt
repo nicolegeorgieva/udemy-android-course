3363bebe02185104317acbadb7318a45003e16d3
Nicole <nicole.georgievva@gmail.com>
2022-11-06 16:45:03 +0200

    Exercise classes

--- Test/src/main/java/com/example/test/MyClass.kt
+++ Test/src/main/java/com/example/test/MyClass.kt
@@ -1,11 +1,18 @@
 package com.example.test
 
 fun main() {
-    val car = Car()
-    print(car.color)
+    val person = Person("Iliyan", 25)
+    val person2 = Person("Nicole", 23)
+
+    printPerson(person)
+    printPerson(person2)
 }
 
-class Car {
-    val color: String = "Red"
-}
+class Person(
+    val name: String,
+    val age: Int
+)
 
+fun printPerson(person: Person) {
+    println("${person.name} is ${person.age} years old.")
+}
