34a99059144ee85e752de885b187dd5259fa12fe
Nicole <nicole.georgievva@gmail.com>
2022-11-06 17:26:00 +0200

    Exercise classes

--- Test/src/main/java/com/example/test/MyClass.kt
+++ Test/src/main/java/com/example/test/MyClass.kt
@@ -2,18 +2,10 @@ package com.example.test
 
 fun main() {
     val car = Car(color = "Red", model = "jsdlf")
-    car.speed(minSpeed = 30, maxSpeed = 130)
+    println(car)
 }
 
-class Car(var color: String, var model: String) {
-    init {
-        if (color == "Green") {
-            println("Yayy Green!")
-        } else {
-            println("$color is not Green.")
-        }
-    }
-
+data class Car(var color: String, var model: String) {
     fun speed(minSpeed: Int, maxSpeed: Int) {
         println("Min speed is $minSpeed, max speed is $maxSpeed.")
     }
