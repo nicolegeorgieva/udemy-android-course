3cb9519dea53a0cdff32be111e5644d374166d80
Nicole <nicole.georgievva@gmail.com>
2022-11-07 23:40:50 +0200

    Exercise interfaces

--- Test/src/main/java/com/example/test/MyClass.kt
+++ Test/src/main/java/com/example/test/MyClass.kt
@@ -3,11 +3,20 @@ package com.example.test
 fun main() {
     val button = Button("Back Button")
     button.onClick("This is a Back Button.")
+
+    val character = Character("Morgana")
+    character.onClick("This is Morgana!")
 }
 
 class Button(val label: String) : ClickEvent {
     override fun onClick(message: String) {
         println("Clicked by $label and here's a message: $message")
+    }
+}
+
+class Character(val name: String) : ClickEvent {
+    override fun onClick(message: String) {
+        println("Clicked by $name and here's a message: $message")
     }
 }
 
