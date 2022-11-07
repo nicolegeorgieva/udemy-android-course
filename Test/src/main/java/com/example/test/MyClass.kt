f81ac0d4248a0636c91a7890ceabf19865138117
Nicole <nicole.georgievva@gmail.com>
2022-11-07 23:34:24 +0200

    Exercise interfaces

--- Test/src/main/java/com/example/test/MyClass.kt
+++ Test/src/main/java/com/example/test/MyClass.kt
@@ -1,33 +1,16 @@
 package com.example.test
 
 fun main() {
-    val car = Car(color = "Red", model = "jsdlf")
-    println(
-        "Car color: ${car.color}, model: ${car.model}"
-    )
-
-    val truck = Truck(color = "Magenta", model = "kjnm")
-    truck.drive()
-    truck.speed(16, 100)
+    val button = Button("Back Button")
+    button.onClick("This is a Back Button.")
 }
 
-open class Car(var color: String, var model: String) {
-    open fun speed(minSpeed: Int, maxSpeed: Int) {
-        println("Min speed is $minSpeed, max speed is $maxSpeed.")
-    }
-
-    open fun drive() {
-        println("Drive...vrooommm")
+class Button(val label: String) : ClickEvent {
+    override fun onClick(message: String) {
+        println("Clicked by $label and here's a message: $message")
     }
 }
 
-class Truck(color: String, model: String) : Car(color, model) {
-    override fun speed(minSpeed: Int, maxSpeed: Int) {
-        val fullSpeed = minSpeed * maxSpeed
-        println("Truck full speed: $fullSpeed.")
-    }
-
-    override fun drive() {
-        println("Vrooommm...like a truck!")
-    }
+interface ClickEvent {
+    fun onClick(message: String)
 }
