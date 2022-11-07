226d708a85a14e110f756bb01eefa1a0acb12c01
Nicole <nicole.georgievva@gmail.com>
2022-11-07 23:00:02 +0200

    Exercise inheritance

--- Test/src/main/java/com/example/test/MyClass.kt
+++ Test/src/main/java/com/example/test/MyClass.kt
@@ -1,12 +1,17 @@
 package com.example.test
 
 fun main() {
-    val car = Car(color = "Red", model = "jsdlf")
-    println(car)
+//    val car = Car(color = "Red", model = "jsdlf")
+//    println(car)
+
+    val truck = Truck(color = "Magenta", model = "kjnm")
+    truck.speed(16, 100)
 }
 
-data class Car(var color: String, var model: String) {
+open class Car(var color: String, var model: String) {
     fun speed(minSpeed: Int, maxSpeed: Int) {
         println("Min speed is $minSpeed, max speed is $maxSpeed.")
     }
 }
+
+class Truck(color: String, model: String) : Car(color, model)
