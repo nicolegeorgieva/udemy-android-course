d8eb6905262c287425d09c6eb3b74dd1f90a0ec0
Nicole <nicole.georgievva@gmail.com>
2022-11-07 23:15:40 +0200

    Exercise override

--- Test/src/main/java/com/example/test/MyClass.kt
+++ Test/src/main/java/com/example/test/MyClass.kt
@@ -1,17 +1,33 @@
 package com.example.test
 
 fun main() {
-//    val car = Car(color = "Red", model = "jsdlf")
-//    println(car)
+    val car = Car(color = "Red", model = "jsdlf")
+    println(
+        "Car color: ${car.color}, model: ${car.model}"
+    )
 
     val truck = Truck(color = "Magenta", model = "kjnm")
+    truck.drive()
     truck.speed(16, 100)
 }
 
 open class Car(var color: String, var model: String) {
-    fun speed(minSpeed: Int, maxSpeed: Int) {
+    open fun speed(minSpeed: Int, maxSpeed: Int) {
         println("Min speed is $minSpeed, max speed is $maxSpeed.")
+    }
+
+    open fun drive() {
+        println("Drive...vrooommm")
     }
 }
 
-class Truck(color: String, model: String) : Car(color, model)
+class Truck(color: String, model: String) : Car(color, model) {
+    override fun speed(minSpeed: Int, maxSpeed: Int) {
+        val fullSpeed = minSpeed * maxSpeed
+        println("Truck full speed: $fullSpeed.")
+    }
+
+    override fun drive() {
+        println("Vrooommm...like a truck!")
+    }
+}
