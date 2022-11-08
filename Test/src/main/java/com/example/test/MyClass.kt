48db6589c0efb61bd7b780f2a892eb5b8f31814c
Nicole <nicole.georgievva@gmail.com>
2022-11-08 23:20:30 +0200

    Exercise extension functions

--- Test/src/main/java/com/example/test/MyClass.kt
+++ Test/src/main/java/com/example/test/MyClass.kt
@@ -2,8 +2,13 @@ package com.example.test
 
 fun main() {
     println("Hello, ".append("Miss"))
+    println("Nicole".removeFirstAndLastChars())
 }
 
 fun String.append(toAppend: String): String {
     return this.plus(toAppend)
+}
+
+fun String.removeFirstAndLastChars(): String {
+    return this.substring(1, this.length - 1)
 }
