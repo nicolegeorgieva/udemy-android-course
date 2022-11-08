63ae583de0d310a375e5524093f5302690dc37dc
Nicole <nicole.georgievva@gmail.com>
2022-11-08 23:08:30 +0200

    Exercise extension functions

--- Test/src/main/java/com/example/test/MyClass.kt
+++ Test/src/main/java/com/example/test/MyClass.kt
@@ -1,25 +1,9 @@
 package com.example.test
 
 fun main() {
-    val button = Button("Back Button")
-    button.onClick("This is a Back Button.")
-
-    val character = Character("Morgana")
-    character.onClick("This is Morgana!")
+    println("Hello, ".append("Miss"))
 }
 
-class Button(val label: String) : ClickEvent {
-    override fun onClick(message: String) {
-        println("Clicked by $label and here's a message: $message")
-    }
-}
-
-class Character(val name: String) : ClickEvent {
-    override fun onClick(message: String) {
-        println("Clicked by $name and here's a message: $message")
-    }
-}
-
-interface ClickEvent {
-    fun onClick(message: String)
+fun String.append(toAppend: String): String {
+    return this.plus(toAppend)
 }
