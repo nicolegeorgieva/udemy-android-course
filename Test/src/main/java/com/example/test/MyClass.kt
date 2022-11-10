74723321a11576e4fe1a70fd12b0c0ac9a865a43
Nicole <nicole.georgievva@gmail.com>
2022-11-10 20:23:14 +0200

    Exercise data class

--- Test/src/main/java/com/example/test/MyClass.kt
+++ Test/src/main/java/com/example/test/MyClass.kt
@@ -2,7 +2,13 @@ package com.example.test
 
 fun main() {
     val person = Person(name = "Amy", lastName = "Grace", age = 20)
-    println(person)
+    val person2 = Person(name = "Nicole", lastName = "Georgieva", age = 23)
+    val person3 = Person(name = "Iliyan", lastName = "Germanov", age = 25)
+
+    val listOfPeople = listOf(person, person2, person3)
+    listOfPeople.forEach { someone ->
+        println(someone.age)
+    }
 }
 
 data class Person(val name: String, val lastName: String, val age: Int)
