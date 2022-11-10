5500f46dcce71e1a351a1529e100e8b9e98ef4a0
Nicole <nicole.georgievva@gmail.com>
2022-11-10 20:15:29 +0200

    Exercise data class

--- Test/src/main/java/com/example/test/MyClass.kt
+++ Test/src/main/java/com/example/test/MyClass.kt
@@ -1,20 +1,8 @@
 package com.example.test
 
 fun main() {
-    val solution = Solution()
-    print(solution.numJewelsInStones(jewels = "z", stones = "ZZ"))
+    val person = Person(name = "Amy", lastName = "Grace", age = 20)
+    println(person)
 }
 
-class Solution {
-    fun numJewelsInStones(jewels: String, stones: String): Int {
-        var result = 0
-        for (x in stones) {
-            for (y in jewels) {
-                if (x == y) {
-                    result += 1
-                }
-            }
-        }
-        return result
-    }
-}
+data class Person(val name: String, val lastName: String, val age: Int)
