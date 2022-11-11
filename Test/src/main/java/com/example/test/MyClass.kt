112901bbe0a7df9e70d63b22a435e99613c2d893
Nicole <nicole.georgievva@gmail.com>
2022-11-11 14:41:15 +0200

    Exercise advanced concepts - enums

--- Test/src/main/java/com/example/test/MyClass.kt
+++ Test/src/main/java/com/example/test/MyClass.kt
@@ -1,30 +1,20 @@
 package com.example.test
 
 fun main() {
-    val listOfNumbers = listOf(23, 45, 64)
+    val input = Result.ERROR
+    getResult(result = input)
 
-    val person = Person(name = "Nicole")
-    val person2 = Person(name = "Iliyan")
-    val person3 = Person(name = "Sarah")
+}
 
-    val listOfPeople = listOf(person, person2, person3)
-
-
-    val finder = Finder(list = listOfPeople)
-    finder.findItem(element = person) {
-        println("Found $it")
+fun getResult(result: Result) {
+    return when (result) {
+        Result.SUCCESS -> println("Success!")
+        Result.FAILURE -> println("Failure!")
+        Result.ERROR -> println("Error!")
     }
 }
 
-class Finder<T>(private val list: List<T>) {
-    fun findItem(element: T, foundItem: (element: T?) -> Unit) {
-        val itemFound = list.filter {
-            it == element
-        }
-
-        if (itemFound.isEmpty()) foundItem(null) else
-            foundItem(itemFound.first())
-    }
+enum class Result {
+    SUCCESS, FAILURE, ERROR
 }
 
-data class Person(val name: String)
