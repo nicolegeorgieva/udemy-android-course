a7aba38642f208819e2757d3afdeef1dfcb63875
Nicole <nicole.georgievva@gmail.com>
2022-11-11 15:05:41 +0200

    Exercise advanced concepts - states

--- Test/src/main/java/com/example/test/MyClass.kt
+++ Test/src/main/java/com/example/test/MyClass.kt
@@ -1,9 +1,14 @@
 package com.example.test
 
 fun main() {
-    val input = Result.ERROR
-    getResult(result = input)
+    Repository.startFetch()
+    getResult(result = Repository.getCurrentState())
 
+    Repository.finishedFetch()
+    getResult(result = Repository.getCurrentState())
+
+    Repository.error()
+    getResult(result = Repository.getCurrentState())
 }
 
 fun getResult(result: Result) {
@@ -11,10 +16,35 @@ fun getResult(result: Result) {
         Result.SUCCESS -> println("Success!")
         Result.FAILURE -> println("Failure!")
         Result.ERROR -> println("Error!")
+        Result.IDLE -> println("Idle")
+        Result.LOADING -> println("Loading")
+    }
+}
+
+object Repository {
+    private var loadState: Result = Result.IDLE
+    private var dataFetched: String? = null
+
+    fun startFetch() {
+        loadState = Result.LOADING
+        dataFetched = "data"
+    }
+
+    fun finishedFetch() {
+        loadState = Result.SUCCESS
+        dataFetched = null
+    }
+
+    fun error() {
+        loadState = Result.ERROR
+    }
+
+    fun getCurrentState(): Result {
+        return loadState
     }
 }
 
 enum class Result {
-    SUCCESS, FAILURE, ERROR
+    SUCCESS, FAILURE, ERROR, IDLE, LOADING
 }
 
